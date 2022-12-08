726733468541984a27995e9228a45c94ef14d249
Marcos Fabiano <marcos.fabiano67@outlook.com>
2022-12-08 14:57:41 -0300

    forms validate

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -19,11 +19,15 @@ export default function ProductForm() {
       placeholder: "Nome",
     },
     price: {
-      value: "",
+      value: 200,
       id: "price",
       name: "price",
       type: "number",
       placeholder: "Preço",
+      validation: function (value: any) {
+        return Number(value) > 0;
+      },
+      message: "Favor informar um valor positivo",
     },
     imgUrl: {
       value: "",
@@ -35,9 +39,11 @@ export default function ProductForm() {
   });
 
   useEffect(() => {
+    console.log(forms.validate(formData, "name"));
+
     if (isEditing) {
       productService.findById(Number(params.productId)).then((response) => {
-        setFormData(forms.updateAll(formData, response.data))
+        setFormData(forms.updateAll(formData, response.data));
       });
     }
   }, []);
